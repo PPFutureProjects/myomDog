97169627a16915168667c6fd3b564eacbac66311
Hoon Ahn <shahn801@gmail.com>
2017-05-22 00:29:43 +0900

    Slide options for history

--- myomDog/src/app/app.module.ts
+++ myomDog/src/app/app.module.ts
@@ -14,7 +14,7 @@ import { RegisterPage } from '../pages/register/register';
 import { ResetpwdPage } from '../pages/resetpwd/resetpwd';
 
 import { HomePage } from '../pages/home/home';
-import { HealthPage } from '../pages/health/health';
+import { HealthPage, SlideOptions } from '../pages/health/health';
 import { WalkPage } from '../pages/walk/walk';
 import { SettingPage, AddingDogPage, InvitingPage, ChangeInfoPage } from '../pages/setting/setting';
 import { TabsPage } from '../pages/tabs/tabs';
@@ -52,6 +52,7 @@ firebase.initializeApp(firebaseConfig);
 
     HomePage,
     HealthPage,
+    SlideOptions,
     WalkPage,
     SettingPage,
     AddingDogPage,
