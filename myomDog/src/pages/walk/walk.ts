c9c2d7862de61ca2585c46cad2e0bc2f19c03428
seunghoseungho <qkrtmdgh1207@naver.com>
2017-05-29 15:46:25 +0900

    똥값제거

--- myomDog/src/pages/walk/walk.ts
+++ myomDog/src/pages/walk/walk.ts
@@ -59,26 +59,18 @@ export class WalkPage {
             });
           })
         });
-      });
-
-      alert.addInput({  // 이부분은 샘플데이터인데, 이부분을 지우면 이상하게 체크박스 목록에 하얗게 비어서 나옴.
-        type: 'checkbox',
-        label: 'Alderaan',
-        value: 'value1',
-        checked: true
-      });
-
-      alert.addButton('Cancel');
-      alert.addButton({
-        text: 'Okay',
-        handler: data => {
-          console.log('Checkbox data:', data);
-          this.checkboxOpen = false;
-          this.selectedDogs = data;
-        }
-      });
-      alert.present().then(() => {
-        this.checkboxOpen = true;
+        alert.addButton('Cancel');
+        alert.addButton({
+          text: 'Okay',
+          handler: data => {
+            console.log('Checkbox data:', data);
+            this.checkboxOpen = false;
+            this.selectedDogs = data;
+          }
+        });
+        alert.present().then(() => {
+          this.checkboxOpen = true;
+        });
       });
     }
   }
