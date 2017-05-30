61c2b499dc4a1b4d54e2fac2894a0bd3cc245fa6
seunghoseungho <qkrtmdgh1207@naver.com>
2017-05-30 13:51:14 +0900

    다듬더듬..

--- myomDog/src/pages/setting/setting.ts
+++ myomDog/src/pages/setting/setting.ts
@@ -49,13 +49,60 @@ export class SettingPage {
     deletedogModal.present();
   }
 
+  deleteGroupModal(){
+    let deletegroupModal = this.modalCtrl.create(DeleteGroupPage);
+    deletegroupModal.present();
+  }
+
   savefavorite(SelectedValue){
     console.log("Favorite: ", SelectedValue);
-    this.manageService.changMainDog(SelectedValue);
+    this.manageService.changeMainDog(SelectedValue);
   }
 
   logout() {
     this.authService.doLogout();
+  }
+
+}
+
+@Component({
+  templateUrl: './deleteGroup.html'
+})
+export class DeleteGroupPage {
+  userKey: string;
+
+  grouplist: FirebaseListObservable<any[]>;
+  selectedGroup: string
+
+  constructor(public alertCtrl: AlertController, public _viewCtrl: ViewController, public manageService: ManageService, public db: AngularFireDatabase){
+    this.userKey = manageService.userKey;
+    this.grouplist = db.list('/userData/'+this.userKey+'/groups');
+  }
+
+  dismiss(){
+    this._viewCtrl.dismiss();
+  }
+
+  deleteButton(){
+    let confirm = this.alertCtrl.create({
+     title: '확인창',
+     message: '해당 그룹을 정말로 삭제할까요?',
+     buttons: [
+       {
+         text: 'Disagree',
+         handler: () => {
+           console.log('Disagree');
+         }
+       },
+       {
+         text: 'Agree',
+         handler: () => {
+           this.manageService.removeGroup(this.selectedGroup);
+         }
+       }
+     ]
+   });
+   confirm.present()
   }
 
 }
@@ -73,13 +120,13 @@ export class AddingDogPage {
 
   dogage:number;
   grouplist: FirebaseListObservable<any[]>;
-  alreadygroupname: string;
-  adddogsex: string;
+  alreadygroup;
   mealtime: string[];
 
   constructor(public alertCtrl: AlertController, public _viewCtrl: ViewController, public manageService: ManageService, public db: AngularFireDatabase){
     this.userKey = manageService.userKey;
     this.grouplist = db.list('/userData/'+this.userKey+'/groups');
+    this.alreadygroup = null;
   }
 
   dismiss(){
@@ -101,11 +148,18 @@ export class AddingDogPage {
          text: 'Agree',
          handler: () => {
            console.log('Agree');
+           console.log("^^alreadygroup: "+this.alreadygroup);
            console.log("dogname :" + this.dogname);
            console.log("birth :" + this.birth);
-           console.log("sex : "+ this.adddogsex);
+           console.log("sex : "+ this.gender);
            console.log("mealtime: "+ this.mealtime);
-           this.manageService.addDog(this.dogname, this.groupname, this.gender, this.birth);
+           if(this.alreadygroup){
+             console.log("alreadygroup: "+this.alreadygroup);
+            this.manageService.addDogToGroup(this.alreadygroup, this.dogname, this.gender ,this.birth);
+           }  else {
+                console.log("newAdd");
+                this.manageService.addDog(this.dogname, this.groupname, this.gender, this.birth);
+           }
          }
        }
      ]
@@ -234,6 +288,7 @@ export class GoodByePage {
            console.log("del dog"+this.goodbyedog);
            //this.manageService.addDog(this.dogname, this.groupname, this.gender, this.birth);
            //삭제 메소드 goodbyedog변수에 삭제할 강아지key값 가지고 있음.
+           this.manageService.goodbyeDog(this.goodbyedog);
          }
        }
      ]
