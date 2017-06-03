35ee41c32d7762816229f86d8951d8c76fe2b23e
Hoon Ahn <shahn801@gmail.com>
2017-06-03 18:12:41 +0900

    fixing

--- myomDog/src/pages/health/health.ts
+++ myomDog/src/pages/health/health.ts
@@ -7,6 +7,13 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Chart } from 'chart.js'
 
 // import { History } from './History'
+
+/**
+ * Generated class for the Health page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
 @IonicPage()
 @Component({
   selector: 'page-health',
@@ -29,33 +36,26 @@ export class HealthPage {
     {
     this.isAndroid = platform.is('android');
     let firebaseData = db.object('userData/'+this.manageService.userKey, {preserveSnapshot: true});
-    let p = new Promise((resolve,reject)=>{
-      firebaseData.subscribe((snapshot)=>{
-        if(snapshot.val().mainDog) {
-          this.myMainDogKey = snapshot.val().mainDog;
-          console.log("대표개: "+ this.myMainDogKey);
-        }
-        else {
-          console.log("No 대표개");
-        }
-        
-        this.dogHistory = db.list('/dogData/'+this.myMainDogKey+'/history', {
-          query: {
-            orderByChild: 'category',
-            equalTo: this.segSubject
-          }
-        });
-        resolve(this.dogHistory);
-      }), err=>{
-        
+    firebaseData.subscribe((snapshot)=>{
+      if(snapshot.val().mainDog) {
+        this.myMainDogKey = snapshot.val().mainDog;
+        console.log("대표개: "+ this.myMainDogKey);
       }
-    }).then(()=>{
-      this.weekBTN();
-    })
-    
+      else {
+        console.log("No 대표개");
+      }
+
+      this.dogHistory = db.list('/dogData/'+this.myMainDogKey+'/history', {
+        query: {
+          orderByChild: 'category',
+          equalTo: this.segSubject
+        }
+      });
+    });
     //this.myMainDogKey = '-Kkp6_SPSiCNeVWj1z5a';//하드코딩 : 공주 키 값
 
     this.segSubject = new BehaviorSubject(undefined);
+
     var subscription = this.segSubject.subscribe(
       function (x) {
         if(x){
@@ -71,11 +71,12 @@ export class HealthPage {
       function () {
           console.log('Completed');
       });
-      console.log(this.dogHistory);
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Health');
+    this.lineChart = this.getLineChart([], []);
   }
 
   ionViewWillLeave() {
@@ -126,32 +127,14 @@ getTime() 은 밀리세컨드 단위로 변환하는 함수이기 때문에 이 
   weekBTN(){
     let current = new Date();
     let labels = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
-    let d = current.toString().split(' ')[0];
-    console.log(d);
-    if(d=='Mon'){
-      labels = ["Tue","Wed","Thu","Fri","Sat","Sun","Mon"];
-    }else if(d=='Tue'){
-      labels = ["Wed","Thu","Fri","Sat","Sun","Mon","Tue"];
-    }else if(d=='Wed'){
-      labels = ["Thu","Fri","Sat","Sun","Mon","Tue","Wed"];
-    }else if(d=='Thu'){
-      labels = ["Fri","Sat","Sun","Mon","Tue","Wed","Thu"];
-    }else if(d=='Fri'){
-      labels = ["Sat","Sun","Mon","Tue","Wed","Thu","Fri"];
-    }else if(d=='Sat'){
-      labels = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
-    }else if(d=='Sun'){
-      labels = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
-    }
     let param = new Promise((resolve,reject)=>{
       this.dogHistory.subscribe(shots=>{
         let cnt = [0, 0, 0, 0, 0, 0, 0];
         shots.forEach(history=>{
-          if(Math.floor( (current.getTime()-(new Date(history.time)).getTime())/1000/60/60/24 ) <= 7 ){
-            for(let i=0; i<7; i++){
-              if(history.time.split(" ")[0]==labels[i]){
-                cnt[i] += history.content;
-              }
+          // 이번주인지확인...
+          for(let i=0; i<7; i++){
+            if(history.time.split(" ")[0]==labels[i]){
+              cnt[i] += history.content;
             }
           }
         })
@@ -166,166 +149,17 @@ getTime() 은 밀리세컨드 단위로 변환하는 함수이기 때문에 이 
   }
 
   monthBTN(){
-     //var result=((x%4 ==0 && x%100!=0 ) || x%400==0)? "윤년" : "윤년X";
-     let current = new Date();
-      let year = current.getFullYear();
-      let month = current.getMonth();
-      let label;
-      let data;
-      let even = ['1','1','1','1','1','1','1',
-                  '1','1','1','1','1','1','1',
-                  '1','1','1','1','1','1','1',
-                  '1','1','1','1','1','1','1',
-                  '1','1'];
-      let odd = ['1','1','1','1','1','1','1',
-                  '1','1','1','1','1','1','1',
-                  '1','1','1','1','1','1','1',
-                  '1','1','1','1','1','1','1',
-                  '1','1','1']; 
-      let noleap = ['1','1','1','1','1','1','1',
-                  '1','1','1','1','1','1','1',
-                  '1','1','1','1','1','1','1',
-                  '1','1','1','1','1','1','1'
-                  ]; 
-      let leap = ['1','1','1','1','1','1','1',
-                  '1','1','1','1','1','1','1',
-                  '1','1','1','1','1','1','1',
-                  '1','1','1','1','1','1','1',
-                  '1'];                   
-     let param = new Promise((resolve, reject)=>{
-      let result = ((year%4 ==0 && year%100!=0 ) || year%400==0)? "윤년" : "윤년X";
-      if(month%2==0 && month!=2){
-        label = even;
-        data = [0,0,0,0,0,0,0,
-                0,0,0,0,0,0,0,
-                0,0,0,0,0,0,0,
-                0,0,0,0,0,0,0,
-                0,0,0]
-      }
-      else if(month==2 && result=='윤년'){
-        label = leap;
-        data = [0,0,0,0,0,0,0,
-                0,0,0,0,0,0,0,
-                0,0,0,0,0,0,0,
-                0,0,0,0,0,0,0,
-                ]
-      }
-      else if(month==2 && result=='윤년X'){
-        label = noleap;
-        data = [0,0,0,0,0,0,0,
-                0,0,0,0,0,0,0,
-                0,0,0,0,0,0,0,
-                0,0,0,0,0,0,0,
-                0]
-      }
-      else if(month%2!=0 && month!=7){
-        label = odd;
-        data = [0,0,0,0,0,0,0,
-                0,0,0,0,0,0,0,
-                0,0,0,0,0,0,0,
-                0,0,0,0,0,0,0,
-                0,0]
-      }
-      else if(month%2!=0 && month==8){
-        label = even;
-        data = [0,0,0,0,0,0,0,
-                0,0,0,0,0,0,0,
-                0,0,0,0,0,0,0,
-                0,0,0,0,0,0,0,
-                0,0,0]
-      }
-      this.dogHistory.subscribe(shots=>{
-        shots.forEach(history=>{
-          let x = new Date(history.time);
-          if(x.getMonth()==month){
-            data[x.getDate()-1] += history.content;
-          }
-        })
-        resolve(data);
-      }),
-      err=>{
-        reject(err);
-      }
-    }).then(()=>{
-      this.lineChart = this.getLineChart(label, data);
-    })
     this.graph = "month"
+    this.lineChart = this.getLineChart([],[]);
   }
 
   threemonthBTN(){
-    let current = new Date();
-    let year = current.getFullYear();
-    let month = current.getMonth();
-    let label;
-    let data;
-
-    let param = new Promise((resolve, reject)=>{
-
-      this.dogHistory.subscribe(shots=>{
-        if(Math.floor(current.getDate()/7)==0){
-          label = 
-        [
-            month-2+'_1', month-2+'_2', month-2+'_3', month-2+'_4',
-            month-1+'_1', month-1+'_2', month-1+'_3', month-1+'_4',
-            month+'_1', month+'_2', month+'_3', month+'_4',month+1+'_1'
-        ];
-          data = [0,0,0,0,0,0,0,0,0,0,0,0,0];
-        }else if(Math.floor(current.getDate()/7)==1){
-          label = 
-        [
-            month-2+'_1', month-2+'_2', month-2+'_3', month-2+'_4',
-            month-1+'_1', month-1+'_2', month-1+'_3', month-1+'_4',
-            month+'_1', month+'_2', month+'_3', month+'_4',
-            month+1+'_1',month+1+'_2'
-        ];
-          data = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-        }else if(Math.floor(current.getDate()/7)==2){
-          label = 
-        [
-            month-2+'_1', month-2+'_2', month-2+'_3', month-2+'_4',
-            month-1+'_1', month-1+'_2', month-1+'_3', month-1+'_4',
-            month+'_1', month+'_2', month+'_3', month+'_4',
-            month+1+'_1', month+1+'_2', month+1+'_3'
-        ];
-          data = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-        }else if(Math.floor(current.getDate()/7)==3){
-          label = 
-        [
-          month-2+'_1', month-2+'_2', month-2+'_3', month-2+'_4',
-            month-1+'_1', month-1+'_2', month-1+'_3', month-1+'_4',
-            month+'_1', month+'_2', month+'_3', month+'_4',
-            month+1+'_1', month+1+'_2', month+1+'_3', month+1+'_4'
-        ];
-          data = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-        }
-        shots.forEach(history=>{
-          let x = new Date(history.time);
-          if(x.getFullYear()==year && (0 <= month - x.getMonth() && month - x.getMonth() <= 3 )){
-            let day = x.getDate();
-            let pos = Math.floor(day/7);
-            data[(x.getMonth()-month+3)*4 + pos] += history.content;
-          }
-        })
-        resolve(data);
-      }),
-      err=>{
-        reject(err);
-      }
-    }).then(result=>{
-      this.lineChart = this.getLineChart(label, result);
-    })
     this.graph = "3months"
+    this.lineChart = this.getLineChart([],[]);
   }
 
   filterBy(segVal?: string) {
     this.segSubject.next(segVal);
-    if(this.graph == "3months"){
-      this.weekBTN();
-    }else if(this.graph == "month"){
-      this.monthBTN();
-    }else if(this.graph == "week"){
-      this.threemonthBTN();
-    }
   }
 
   editItem(history){
@@ -377,4 +211,16 @@ getTime() 은 밀리세컨드 단위로 변환하는 함수이기 때문에 이 
 
    return this.getChart(this.lineCanvas.nativeElement, "line", data);
  }
+
+ // 추가 버튼 이벤트
+ addFoodHistory(){
+   console.log("addFoodHistory clicked");
+ }
+ addWalkHistory(){
+   console.log("addWalkHistory clicked");
+ }
+ addEtcHistory(){
+   console.log("addEtcHistory clicked");
+ }
+
 }
